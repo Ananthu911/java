201362c2ca6ebe5279ad47bea95794e6145f7c40
Ananthu911 <ananthu0705@gmail.com>
2026-02-09 18:17:17 +0530

    Add files via upload

---
+++ vowel.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class vowel{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    char ch=sc.next().charAt(0);
+    switch(ch){
+      case'A':case'E':case'I':case'O':case'U':
+      case'a':case'e':case'i':case'o':case'u':
+      System.out.println("vowels");break;
+      default:System.out.println("Consonant");
+        
+    }
+  }
+}
